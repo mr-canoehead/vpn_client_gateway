e51879431b2658fcaf3a7e9402a1477669842767
mr_canoehead <mr_canoehead@telus.net>
2015-11-11 11:27:59 -0800

    fixed #11, fixed #12, added new vpn servers.

--- www/vpnmgmt/enablevpn.php
+++ www/vpnmgmt/enablevpn.php
@@ -3,7 +3,9 @@
 // remove disabled marker file
 $result = unlink('/var/www/vpnmgmt/vpn.disabled');
 // set openvpn service to start automatically on boot
-$result = shell_exec('sudo update-rc.d -f openvpn defaults');
+$result = shell_exec('sudo update-rc.d openvpn enable');
+// add nat postrouting rule for tun0
+$result = shell_exec('sudo iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE');
 // remove nat postrouting rule for eth0
 $result = shell_exec('sudo iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE');
 // save iptables

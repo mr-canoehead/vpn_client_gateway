cacfe4d4930162cc8893e6a7a506e84d34cb1e32
Chris Lindstrom <chris@tvpc.mydomain.home>
2016-06-08 18:04:17 -0700

    flush nat postrouting rules in enablevpn.php and disablevpn.php prior to adding new postrouting rules. fixes issue #15

--- www/vpnmgmt/disablevpn.php
+++ www/vpnmgmt/disablevpn.php
@@ -6,10 +6,10 @@ $result = shell_exec('touch /var/www/vpnmgmt/vpn.disabled');
 $result = shell_exec('sudo service openvpn stop');
 // prevent openvpn from starting at boot
 $result = shell_exec('sudo update-rc.d openvpn disable');
+// remove any existing nat postrouting rules
+$result = shell_exec('sudo iptables -t nat -F POSTROUTING');
 // add nat postrouting rule to allow forwarding via eth0
 $result = shell_exec('sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE');
-// remove nat postrouting rule for tun0
-$result = shell_exec('sudo iptables -t nat -D POSTROUTING -o tun0 -j MASQUERADE');
 // save iptables
 $result = shell_exec("sudo su -c 'iptables-save > /etc/iptables/rules.v4'");
 ?>
--- www/vpnmgmt/enablevpn.php
+++ www/vpnmgmt/enablevpn.php
@@ -4,10 +4,10 @@
 $result = unlink('/var/www/vpnmgmt/vpn.disabled');
 // set openvpn service to start automatically on boot
 $result = shell_exec('sudo update-rc.d openvpn enable');
+// remove any existing nat postrouting rules
+$result = shell_exec('sudo iptables -t nat -F POSTROUTING');
 // add nat postrouting rule for tun0
 $result = shell_exec('sudo iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE');
-// remove nat postrouting rule for eth0
-$result = shell_exec('sudo iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE');
 // save iptables
 $result = shell_exec("sudo su -c 'iptables-save > /etc/iptables/rules.v4'");
 ?>

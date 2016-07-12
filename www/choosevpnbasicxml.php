e36f3635c815eba2e11e4b0a1a8012f1ddd604f3
Chris Lindstrom <chris@tvpc.mydomain.home>
2016-07-11 17:09:10 -0700

    switched to open source flag images from the Google internationalization project

--- www/choosevpnbasicxml.php
+++ www/choosevpnbasicxml.php
@@ -16,7 +16,7 @@ foreach($vpnserverinfo->basicvpnservers->servername as $servername){
 	$country = $countryinfo->xpath($xpathquery);
 	$flagfilestr= (string) $country[0]->flagfile;
 	echo "<TD>" . "\n";
-	echo "<A HREF=\".?vpnserver=" . $servernamestr . "\" onclick=\"show_changing_vpn_message();\"><IMG width=40% SRC=\"images/flags/" . $flagfilestr . "\"/></A>" . "\n";
+	echo "<A HREF=\".?vpnserver=" . $servernamestr . "\" onclick=\"show_changing_vpn_message();\"><IMG height=60% SRC=\"images/flags/" . $flagfilestr . "\"/></A>" . "\n";
 	echo "<P>" . $countrynamestr;
 	if ($regionstr<>"") echo "<br>(" . $regionstr . ")";
 	echo "</P>\n";

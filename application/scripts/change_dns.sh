466514775a193f6846b0562a0c3b4e8f689569ca
Chris <mr_canoehead@outlook.com>
2021-04-01 13:59:18 -0700

    added optional commands for clearing the resolver cache and restarting hostapd (the WiFi access point service)

--- application/scripts/change_dns.sh
+++ application/scripts/change_dns.sh
@@ -1,26 +1,37 @@
 #!/bin/bash
 
+#--------------------------------------------------------------------------------------
+# This script is run each time the VPN state is changed (enable/disable/change server).
+# It is called via 'up' and 'down' directives in the OpenVPN configuration file.
+# The script updates the resolver configuration file with the appropriate DNS server
+# settings, and can also run optional commands to flush the DNS cache and to restart
+# the WiFi access point service (hostapd).
+#
+# When configuring your system you should uncomment the appropriate settings for
+# SERVERS_VPN_CONNECTED and SERVERS_VPN_DISCONNECTED.
+#--------------------------------------------------------------------------------------
+
 #dnsmasq nameserver file
 RESOLVCONF=/etc/dnsmasq-resolv.conf
 
 ### DNS servers to use when VPN is connected
 
-#PIA DNS+Streaming+MACE
-#SERVERS_VPN_CONNECTED=(10.0.0.241)
+##PIA DNS+Streaming+MACE
+SERVERS_VPN_CONNECTED=(10.0.0.241)
 
-#PIA DNS
+##PIA DNS
 #SERVERS_VPN_CONNECTED=(10.0.0.242)
 
-#PIA DNS+Streaming
-SERVERS_VPN_CONNECTED=(10.0.0.243)
+##PIA DNS+Streaming
+#SERVERS_VPN_CONNECTED=(10.0.0.243)
 
-#PIA DNS+MACE
+##PIA DNS+MACE
 #SERVERS_VPN_CONNECTED=(10.0.0.244)
 
-#Surfshark
+##Surfshark
 #SERVERS_VPN_CONNECTED=(162.252.172.57 149.154.159.92)
 
-#NordVPN
+##NordVPN
 #SERVERS_VPN_CONNECTED=(103.86.96.100 103.86.99.100)
 
 ### DNS servers to use when VPN is not connected
@@ -44,3 +55,25 @@ done
 printf "$output" | tee > $RESOLVCONF
 
 
+#-------------------------------------------------------------------------------
+# The following are commands that may be useful to run when changing VPN servers
+# or enabling/disabling the VPN.
+# Uncomment the appropriate commands for your installation type.
+#-------------------------------------------------------------------------------
+
+#-----------------------------------------------------------------
+# Clear the DNS resolver cache by restarting the resolver service.
+#-----------------------------------------------------------------
+
+## Standard installations without Pi-hole:
+#systemctl restart dnsmasq
+
+## Installations with Pi-hole:
+#/usr/local/bin/pihole restartdns
+
+#--------------------------------------------------------------------
+# Restart the hostapd service; this forces WiFi clients to reconnect.
+# This may help with clearing the DNS cache on client devices.
+#--------------------------------------------------------------------
+
+#systemctl restart hostapd
